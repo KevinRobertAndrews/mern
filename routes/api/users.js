9296925530aa9f52a2336dc0d290202ff8636473
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-08-21 16:55:54 -0400

    Make server return a JWT on successfully registered user

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const { check, validationResult } = require("express-validator");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../../models/User");
 
@@ -48,7 +50,22 @@ router.post(
 			await user.save();
 
 			// Return JWT
-			res.send("Use registered");
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 360000,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json(token);
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error.");
