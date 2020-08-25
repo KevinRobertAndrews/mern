c7ce1d8b1560453f329ac5bd6bd1e19890b0c43a
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-08-25 15:35:28 -0400

    Add react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "../src/components/Layout/Navbar";
 import Landing from "../src/components/Layout/Landing";
+import Login from "../src/components/auth/Login";
+import Register from "../src/components/auth/Register";
 
 const App = () => (
-	<Fragment>
-		<Navbar />
-		<Landing />
-	</Fragment>
+	<BrowserRouter>
+		<Fragment>
+			<Navbar />
+			<Route exact path="/" component={Landing} />
+			<section className="container">
+				<Switch>
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/login" component={Login} />
+				</Switch>
+			</section>
+		</Fragment>
+	</BrowserRouter>
 );
 
 export default App;
