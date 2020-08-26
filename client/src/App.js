03d946c9adf9551f33d61a8822917de6580fab6e
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-08-26 13:05:15 -0400

    Build redux store and alert reducers

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,31 @@ import React, { Fragment } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
+// Components
 import Navbar from "../src/components/Layout/Navbar";
 import Landing from "../src/components/Layout/Landing";
 import Login from "../src/components/auth/Login";
 import Register from "../src/components/auth/Register";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => (
-	<BrowserRouter>
-		<Fragment>
-			<Navbar />
-			<Route exact path="/" component={Landing} />
-			<section className="container">
-				<Switch>
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/login" component={Login} />
-				</Switch>
-			</section>
-		</Fragment>
-	</BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
